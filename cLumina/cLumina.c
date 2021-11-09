e43badbc3938e1ba4b50419cb39a5abce7b7f525
TheLazyDutchmen <jjpouwels2004@gmail.com>
2021-11-09 09:00:17 +0100

    Adding '-dump' flag

--- cLumina/cLumina.c
+++ cLumina/cLumina.c
@@ -1,26 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "cLumina.h"
 #include "parser.h"
 
 void usage() {
-	printf("usage: cLumina inputFile\n");
+	printf("usage: cLumina [-dump] [-r] <inputFile>\n");
+	printf("	-dump: dumps the lexer output to stdout\n");
+	printf("	-r: run the generated executable after compilation finished\n");
 }
 
 int main(int argc, char *argv[]) {
-	if (argc < 2) {
-		usage();
-		return -1;
+	ParseFlag flags = 0;
+	char* fileName = NULL;
+
+	argv++;
+	while (*argv != NULL) {
+		if (strcmp(*argv, "-dump") == 0) {
+			flags |= FLAG_DUMP;
+		}
+		else {
+			fileName = *argv;
+		}
+		argv++;
 	}
 
-	char *fileName = argv[1];
-
-	ParseFlag flags;
-
-	flags |= FLAG_DUMP;
+	if (fileName == NULL) {
+		printf("[ERROR] expected fileName\n");
+		usage();
+		exit(1);
+	}
 
 	Parser* parser = initParser(fileName, flags);
 	parse(parser);
 
+	//TODO: freeParser causes a segfault (probably tries to free a null pointer somewhere
+
 	return 0;
+
 }
