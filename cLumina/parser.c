1a4c9b588ce4996077d75a92434de9c527d4f87b
TheLazyDutchmen <jjpouwels2004@gmail.com>
2021-11-19 17:18:55 +0100

    added panic mode

--- cLumina/parser.c
+++ cLumina/parser.c
@@ -84,6 +84,13 @@ ParseRule parseTable[] = {
 	[TOKEN_END_OF_FILE] = {NULL, NULL, PREC_NONE}
 };
 
+void panic(Parser* parser) {
+	Tokentype type = parser->current->type;
+	while (type != TOKEN_SEMICOLON && type != TOKEN_RBRACE && type != TOKEN_RPAREN && type != TOKEN_END_OF_FILE) {
+		type = next(parser)->type;
+	}
+}
+
 void parseError(Parser* parser, Token token, char* message) {
 	char* word = strndup(token.word, token.wordLen);
 	printf("%s:%d ERROR at '%s': ", token.fileName, token.line, word);
@@ -93,7 +100,8 @@ void parseError(Parser* parser, Token token, char* message) {
 	free(word);
 
 	parser->hadError = true;
-	//TODO: enter panic mode
+
+	panic(parser);
 }
 
 Token parsePrecedence(Parser* parser, Precedence precedence) {
