b59d56c85be88282c4ed7d481b6a24f39346ce46
TheLazyDutchmen <jjpouwels2004@gmail.com>
2021-11-20 17:26:14 +0100

    filter files with '.lumina' extension

--- tests/test.c
+++ tests/test.c
@@ -1,11 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <dirent.h>
+#include <string.h>
+
+int filterExtension(const struct dirent *file) {
+	if (!file) {
+		return 0;
+	}
+
+	char* extension = strrchr(file->d_name, '.');
+
+	if (!extension || extension == file->d_name) {
+		return 0;
+	}
+
+	if (strcmp(extension, ".lumina") == 0) {
+		return 1;
+	}
+
+	return 0;
+}
 
 int main(int argc, char** argv) {
 	struct dirent **nameList;
 
-	int n = scandir(".", &nameList, NULL, alphasort);
+	int n = scandir(".", &nameList, filterExtension, alphasort);
 
 	if (n == -1) {
 		perror("open dir");
