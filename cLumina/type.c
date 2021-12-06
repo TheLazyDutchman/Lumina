85d82a022fafd6a05ec20b7b33e7bd92afd6dd1f
TheLazyDutchmen <jjpouwels2004@gmail.com>
2021-12-06 16:47:09 +0100

    added type struct

--- cLumina/type.c
+++ cLumina/type.c
@@ -1,1 +1,15 @@
+#include "stdlib.h"
+#include "string.h"
+
 #include "type.h"
+
+Type *initType(char* name) {
+	Type *type = malloc(sizeof(Type));
+
+	type->name = strdup(name);
+}
+
+void freeType(Type *type) {
+	free(type->name);
+	free(type);
+}
--- cLumina/type.h
+++ cLumina/type.h
@@ -1,4 +1,11 @@
 #ifndef cLumina_type_header
 #define cLumina_type_header
 
+typedef struct {
+	char* name;
+} Type;
+
+Type *initType(char* name);
+void freeType(Type *type);
+
 #endif
