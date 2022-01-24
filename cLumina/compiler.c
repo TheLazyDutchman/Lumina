9a6041b16acf53411b657e4cf658d492e40e7623
TheLazyDutchmen <jjpouwels2004@gmail.com>
2022-01-24 15:42:02 +0100

    init type list in compiler

--- cLumina/compiler.c
+++ cLumina/compiler.c
@@ -9,6 +9,7 @@ Compiler* initCompiler(FILE* output, Compiler* outer) {
 	compiler->currentStackSize = 0;
 	compiler->variableList = initVariableList();
 	compiler->functionList = initFunctionList();
+	compiler->typeList = initTypeList();
 	compiler->hasReturned = false;
 	compiler->outer = outer;
 
